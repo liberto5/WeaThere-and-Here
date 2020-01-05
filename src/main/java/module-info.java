b1ee0b1277f9debb2b4489dd9ffea7a0153518b5
AdamGolub <adam.golub.programista@gmail.com>
2020-01-05 11:46:42 +0100

    Suggest city name under the text field

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module it.adamgolub.WeaThereAndHere {
-    requires javafx.controls;
-    requires javafx.fxml;
     requires owm.japis;
     requires java.sql;
     requires annotations;
     requires com.google.gson;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires org.controlsfx.controls;
+    requires javafx.fxml;
 
     opens it.adamgolub.WeaThereAndHere to javafx.fxml;
     exports it.adamgolub.WeaThereAndHere;
